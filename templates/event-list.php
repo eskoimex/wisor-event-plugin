53dc9edd3acf348e98ff36b0c99345ce5e72e5bb
eskoimex <samuel.imex@gmail.com>
2025-03-21 03:47:13 +0100

    added the template that renders the list of events

---
+++ templates/event-list.php
@@ -0,0 +1,13 @@
+<?php if ($query->have_posts()) : ?>
+    <ul class="wisor-events-list">
+        <?php while ($query->have_posts()) : $query->the_post(); ?>
+            <li>
+                <h3><?php the_title(); ?></h3>
+                <p><?php the_content(); ?></p>
+                <span><?php echo get_post_meta(get_the_ID(), 'event_date', true); ?></span>
+            </li>
+        <?php endwhile; ?>
+    </ul>
+<?php else : ?>
+    <p>No upcoming events found.</p>
+<?php endif; ?>
