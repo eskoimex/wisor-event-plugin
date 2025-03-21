bdf5022fc4004873df9887966f6021c3f89ca7a3
Ime Contribution to MTN Retail POS project <ime.samuel@blusalt.net>
2025-03-21 02:36:19 +0100

    added the plugin header to register it with wordPress

---
+++ wisor-events-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wisor Events Plugin
+Description: A custom plugin to manage and display upcoming events using Elementor and shortcodes.
+Version: 1.0
+Author: Your Name
+*/
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
